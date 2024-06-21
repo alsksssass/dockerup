ddc3caa4febc7cafea113709a8fb766f8083064e
hoone0802 <sabyun@student.42seoul.kr>
2024-06-21 16:55:40 +0900

    aa

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ ! -f "/etc/nginx/ssl/server.crt" ] || [ ! -f "/etc/nginx/ssl/server.key" ]; then
-	openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=sungyoon/OU=Sungyoon/CN=KR" -keyout /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt
+	openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=sabyun/OU=sabyun/CN=KR" -keyout /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt
 fi
 
 exec nginx -g "daemon off;"
